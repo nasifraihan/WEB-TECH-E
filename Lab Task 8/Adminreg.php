b492112d137bef1f18e9099bb6afaad408730328
Nasif Raihan <72753219+nasifraihan@users.noreply.github.com>
2020-12-17 23:48:54 +0600

    Update Adminreg.php

--- Lab Task 8/Adminreg.php
+++ Lab Task 8/Adminreg.php
@@ -11,7 +11,7 @@
 
 <script type = "text/javascript">
    <!--
-      // Form validation code will come here.
+      
       function validate() {
       
          if( document.myForm.name.value == "" ) {
